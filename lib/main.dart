c534c621daf3711ecc1534707d48bfb1ab2a8fa2
mohamedrashad102 <mmm682552@gmail.com>
2024-07-20 15:59:42 +0300

    Add Firebase Core dependency and configuration files
    This commit adds the Firebase Core dependency to the project and configures it for both Android and iOS platforms. It also includes the necessary configuration files for Firebase, including `google-services.json` and `GoogleService-Info.plist`. Additionally, a `firebase_options.dart` file is added to provide default Firebase options for the app. Finally, the necessary changes are made to the `main.dart` file to initialize Firebase and the `GeneratedPluginRegistrant` files for Android, iOS, and Windows are updated to include the Firebase Core plugin.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/utils/app_router.dart';
 
 import 'core/cached_data/cached_data.dart';
+import 'firebase_options.dart';
 import 'generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await Future.wait([
     CachedData.init(),
   ]);
@@ -26,7 +32,6 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      
       locale: const Locale('ar'),
       theme: ThemeData.light().copyWith(
         scaffoldBackgroundColor: Colors.white,
